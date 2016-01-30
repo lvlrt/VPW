abf36863acbcec0cd545f08ca31ee3a834feda96
AsselberghP <philippeas@hotmail.com>
2016-01-30 19:34:19 +0100

    Speciaal voor Lars

--- Vluchten/vluchten.py
+++ Vluchten/vluchten.py
@@ -6,7 +6,7 @@
 n = int(raw_input())
 
 """Getallen inlezen"""
-for j in range(n+1):
+for j in range(n):
     """Aantal vluchten in testgeval"""
     aantalVluchten, aantalVliegtuigen = raw_input().rstrip().split( )
     vluchten = [[0 for x in range(2)] for x in range(int(aantalVluchten))]
@@ -14,4 +14,13 @@ for j in range(n+1):
     for i in range(int(aantalVluchten)):
         vluchten[i][0], vluchten[i][1] = raw_input().rstrip().split( )
     for i in range(int(aantalVliegtuigen)):
-        print 1
+        vliegtuigen[i][0], vliegtuigen[i][1] = raw_input().rstrip().split( )
+    if int(aantalVluchten) > int(aantalVliegtuigen):
+        print j+1, "geen oplossing"
+    oplossing = False
+    for i in range(int(aantalVluchten)):
+        for j in range(int(aantalVliegtuigen)):
+            if int(vliegtuigen[j][0]) > int(vluchten[i][0]) and int(vliegtuigen[j][1]) > int(vluchten[i][1]):
+                oplossing = True
+    if oplossing == False:
+        print j+1, "geen oplossing"
