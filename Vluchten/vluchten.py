3b2cd242f06eaeaffa55ef69d77929a665534382
AsselberghP <philippeas@hotmail.com>
2016-01-30 19:05:18 +0100

    Speciaal voor Lars

--- Vluchten/vluchten.py
+++ Vluchten/vluchten.py
@@ -8,29 +8,10 @@ n = int(raw_input())
 """Getallen inlezen"""
 for j in range(n+1):
     """Aantal vluchten in testgeval"""
-    aantalVluchten = int(raw_input())
-    ts = [[0 for x in range(4)] for x in range(aantalRechthoek)]
-    for i in range(aantalRechthoek):
-        rechthoek=raw_input()
-        ts[i] = rechthoek.split( )
-    x = xComponentRechthoek(ts)
-    y = yComponentRechthoek(ts)
-    num_list = [[0 for z in range(y)] for z in range(x)]
-    for i in range(aantalRechthoek):
-        for j in range(int(ts[i][0]),int(ts[i][0])+int(ts[i][2])):
-            for k in range(int(ts[i][1]),int(ts[i][1])+int(ts[i][3])):
-                num_list[j][k] = 1
-
-    """bereken omtrek"""
-    om = berekenOmtrek(num_list)
-    """bereken oppervlakte"""
-    op = oppervlakte(num_list)
-
-    """omtrek"""
-    out1.append(om)
-    """oppervlakte"""
-    out2.append(op)
-
-for x in range(n+1):
-    """nummer _ oppervlakte _ omtrek"""
-    print x+1, out2[x] , out1[x]
+    aantalVluchten, aantalVliegtuigen = raw_input().rstrip().split( )
+    vluchten = [[0 for x in range(2)] for x in range(int(aantalVluchten))]
+    vliegtuigen = [[0 for x in range(2)] for x in range(int(aantalVliegtuigen))]
+    for i in range(int(aantalVluchten)):
+        vluchten[i][0], vluchten[i][1] = raw_input().rstrip().split( )
+    for i in range(int(aantalVliegtuigen)):
+        print 1
